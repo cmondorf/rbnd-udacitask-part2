2dc44e717c44cfb94efadf2e183d91e2c511bfbf
Christian Mondorf <christian@mondorf.dk>
2016-04-04 11:11:26 +0200

    now working with ljust

--- lib/link.rb
+++ lib/link.rb
@@ -6,13 +6,11 @@ class LinkItem
     @description = url
     @site_name = options[:site_name]
   end
-  def format_description
-    "#{@description}".ljust(25)
-  end
+
   def format_name
     @site_name ? @site_name : ""
   end
   def details
-    format_description + "site name: " + format_name
+    format_description(@descrption) + "site name: " + format_name
   end
 end
